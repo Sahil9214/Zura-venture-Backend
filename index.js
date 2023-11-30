3a961a9090e13b5a6fd44af6f2fe819c2b05a513
Utkarsh <utkarshsinghal369@gmail.com>
2023-11-30 20:30:01 +0530

    add routes

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const connection=require("./configs/db")
 require("dotenv").config()
 const cors=require("cors")
 
-const  projectRouter= require("./route/project.route")
-const projectfileRouter=require("./route/projectfile.route")
+const  projectRouter= require("./route/project.Route")
+const projectfileRouter=require("./route/projectfile.Route")
 
 const app=express()
 app.use(express.json())
