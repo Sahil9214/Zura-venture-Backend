d92143b9474e1d42e462acb80325ecd9b0ec124b
Utkarsh <utkarshsinghal369@gmail.com>
2023-11-30 22:05:31 +0530

    Backedn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const connection=require("./configs/db")
 require("dotenv").config()
 const cors=require("cors")
 
-const  projectRouter= require("./route/project.Route")
-const projectfileRouter=require("./route/projectfile.Route")
+// const  projectRouter= require("./route/project.Route")
+// const projectfileRouter=require("./route/projectfile.Route")
 
 const app=express()
 app.use(express.json())
